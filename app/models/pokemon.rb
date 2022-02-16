44890d33d95fa99456e30671c8d7fb6cbcc2934c
Reece McTiernan <rmctiernan@pm.me>
2022-02-16 14:20:41 +0000

    Add some styling and contact info

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,4 +3,6 @@ class Pokemon < ApplicationRecord
   validates :pokedex_id, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 898} # TODO Will need updated when new pokemon are available 
   belongs_to :link_row
 
+  # TODO: CSV Export feature just to get pokemon names, pokedex id, and links?
+
 end
