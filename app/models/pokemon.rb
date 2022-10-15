0612fc3a4ca4fa79456f0470df4ff78b349fe277
Reece McTiernan <rmctiernan@protonmail.com>
2022-10-15 01:15:24 +0100

    Add turbo stream for pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon < ApplicationRecord
   belongs_to :team, touch: true
-  delegate :game, to: :employee
-  validates :pokedex_id, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 898} # TODO Will need updated when new pokemon are available
+  delegate :game, to: :team
+  validates :pokedex_id, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 898 } # TODO Will need updated when new pokemon are available
   belongs_to :link_row
 
   attribute :nickname, :string, default: ""
@@ -10,9 +10,34 @@ class Pokemon < ApplicationRecord
 
   scope :alive, -> { where(is_alive: true) }
   scope :dead, -> { where(is_alive: false) }
-  scope :boxed, -> { where(is_boxed: true) }
+  scope :boxed_and_alive, -> { where(is_boxed: true, is_alive: true) }
   scope :order_by_id, -> { order(:id) }
+
+  def alive?
+    is_alive
+  end
+
+  def dead?
+    !alive?
+  end
+
+  def boxed?
+    is_boxed
+  end
 
   # TODO: CSV Export feature just to get pokemon names, pokedex id, and links?
 
+  after_update_commit do
+    broadcast_remove_to(game)
+    if alive?
+      broadcast_append_to(game, target: "alive-pokemons", locals: { game: game, team: team })
+    elsif dead?
+      broadcast_append_to(game, target: "dead-pokemons", locals: { game: game, team: team })
+    elsif boxed?
+      broadcast_append_to(game, target: "boxed-pokemons", locals: { game: game, team: team })
+    end
+  end
+
+  after_destroy_commit { broadcast_remove_to(game) }
+  after_create_commit { broadcast_append_to(game, locals: { game: game, team: team }) }
 end
